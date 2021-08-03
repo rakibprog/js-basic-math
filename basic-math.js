82a93748a6db4bb22a316bb26efc87d802ee9ad7
rakibprog <contact.engrakib@gmail.com>
2021-08-03 15:15:46 +0600

    js basci math

---
+++ basic-math.js
@@ -0,0 +1,53 @@
+// js function add 
+
+function add(num1, num2) {
+    var total = num1 + num2;
+    return total;
+}
+
+var result = add(50, 100);
+console.log(result);
+
+
+//js function multiple 
+
+function multi(num1, num2) {
+
+    var total = num1 * num2;
+    return total;
+}
+
+
+var result = multi(10, 20);
+console.log(result);
+
+
+// js function  subtraction 
+
+function sub(num1, num2) {
+    var total = num1 - num2;
+    return total;
+}
+
+var result = sub(100, 50);
+console.log(result);
+
+
+// js function divide 
+
+function divide(num1, num2) {
+    var total = num1 / num2;
+    return total;
+}
+var result = divide(100, 200);
+console.log(result);
+
+// js function modulas 
+
+function modu(num1, num2) {
+    var total = num1 % num2;
+    return total;
+}
+
+var result = modu(100, 200);
+console.log(result);
